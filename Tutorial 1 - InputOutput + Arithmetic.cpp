b60ac4c4a6f33ff2140bc51ea16aad2149c5ef02
ScarletMcLearn <ratul_shams@yahoo.com>
2017-03-10 20:04:40 +0600

    Add files via upload

---
+++ Tutorial 1 - InputOutput + Arithmetic.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+
+
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+	// cout<<"Ek Deshe Ak Raja Chilo. \nShei Rajar Chilo Ek Theng."<<endl<<"Teng Tenga Teng Teng!"<<endl;
+
+	// cout<<endl<<"Amar Kache Ache " <<10 <<" Ta Kukur Chana." <<endl;
+	// cout<<"\nKedir Mullar Kache Ache "<<4<<" Ta Kukur Chana."<<endl;
+	// cout<< "Amar Kache Akhon Ache "<<10 + 4 << " Ti Kukur Chana, Ar Kedir Mullah Jele. ^.^ " <<endl <<endl;
+
+
+	// cout<<"Cholen Vi O Boien-era, Kichu Met Hoiyya Jak." <<endl<<endl;
+
+	// cout<<"2 + 2 = " << 2 + 2 << endl;
+
+	// cout<<"2 * 3 = " << 2 *3 << endl;
+
+	// cout<<"2 - 3 = " << 2 - 3 << endl;
+
+	// cout<<"3 / 2 = " << 3/2 << endl;
+
+	// cout<<"2 / 3 = " << 2 / 3 << endl;
+
+
+
+
+
+	// cout<<endl<<1 << " Ti Bitchy Ola Kolar Daam " << 12.5 << " Teka. \n";
+
+	// cout<<"Mamar Koyta Kola Laigbo? " <<endl;
+
+	// cout<<"Amar Lagbo: ";
+
+	// int number_of_kolas;
+
+	// cin >> number_of_kolas;
+
+	// cout<<endl<< number_of_kolas << " Guli Kolar Jonne Mamar Account-e " << number_of_kolas * 12.5 << " Teka Thakte Hope. " <<endl;
+
+
+
+	// cout<<endl<<endl<<"Robir Intarnet Ret Hocca Tk.15.2. " <<endl;
+	// cout<<"Jhulir Balance Assey: ";
+
+	// int balance;
+	// cin>> balance;
+
+	// cout<<"Jhuli " << balance/15.2 << " Minite-er Jonno IstuPitsu Korte Purbeh Ajkeh ;) " <<endl;
+
+
+
+	// cout<<endl<<"Apnar SIM Er Rate Koto Bhaa?" <<endl;
+
+	// int bhaa_er_rate;
+	// cout<<"Bhaa Er Rate: ";
+
+	// cin>>bhaa_er_rate;
+
+	// cout<<endl<<"Apne Kotokkhon Kotha Koite San? " <<endl;
+	// cout<<"Bhaa Kotha Koite Cay: ";
+
+	// int bhaa_er_time;
+	// cin>>bhaa_er_time;
+
+	// cout<<"Ettokhon Kotha Koite Apnar Lagbo ";
+
+	// int teka = bhaa_er_time * bhaa_er_rate;
+
+	// cout<<teka << " Teka. \n" <<endl;
+
+
+
+
+	// cout<<endl<<"Ekta Numbar Dao, Ami Megix Dekhabo. " <<endl;
+	// cout<<"Nambar: "<<endl;
+
+	// int nambar;
+	// cin>> nambar;
+
+	// cout<<"\nTor Nambar Hocca " << nambar << ". ;) " <<endl;
+
+
+
+
+	// //Insert Phase Here Dawg - to get dexno.
+
+
+	// cout<<"Ekta Variable-er Size Amra Dekte Pari sizeof() function diya. " <<endl <<endl;
+
+	// cout<<"Integer Er Size Hola " << sizeof(int) << " Bytes. " <<endl;
+	// cout<<"Float Er Size Hola " << sizeof(float) << " Bytes. " <<endl;
+	// cout<<"Double Er Size Hocca " << sizeof(double) << " Bytes. " <<endl;
+	// cout<<"String Er Size Hoitasa " << sizeof(string) << " Bytes. " <<endl;
+	// cout<<"Character Er Size Hoitese " << sizeof(char) << " Bytes. " <<endl;
+	// cout<<"Boolean Er Size Hola " << sizeof(bool) << " Bytes. " <<endl;
+
+
+
+
+	// cout<<"Ekhon Amra Notun Jinsih Dekbo Guys. \nEktu Sirius Howa Fake Kori. :3" <<std::endl;
+
+	// cout<<"a^b Kemne Kora Jay Computer Diye? \n(a^b Mane a Er Mathar Upre Ekta Picchi b Asse [a to the power of b])" <<endl;
+
+	// cout<<endl<<"Eitar Jonno Amger Lagbe Ekti Ishpeshal Library Jinar Naam Hocca CMath Library. (Google How To Import) " <<endl;
+
+	// cout<<"\nPlease Enter the value of the base: ";
+
+	// float base;
+
+	// cin>> base;
+
+	// cout<<"Please Enter the value of the power: ";
+
+	// float power;
+
+	// cin>> power;
+
+	// float a_to_the_power_of_b = pow(base, power);
+
+	// cout<<endl<<base<< " Raised To The "<< power << "th Power Izikaltu " << a_to_the_power_of_b << ". "<<endl<<endl;
+
+
+
+	
+	return 0;
+}
+
