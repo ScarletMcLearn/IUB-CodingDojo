cc1f9e86fd584c0362961d9d67957a1af76fb9bc
ScarletMcLearn <ratul_shams@yahoo.com>
2017-03-24 18:10:00 +0600

    Add files via upload

---
+++ Tutorial 2 - If Else.cpp
@@ -0,0 +1,217 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+//    Phase 1 :
+
+//    cout<<"23 > 3 = " << (23 > 3) << endl;
+//
+//    cout<<"12 > 233 = " << (12 > 233) << endl;
+//
+//    cout<<"4 == 4 = " << (4 == 4) << endl;
+//
+//    cout<<"4 <= 4 = " << (4 <= 4) << endl;
+//
+//    cout<<"4 >= 4 = " << (4 >= 4) << endl;
+//
+//    cout<<"3 >= 2 = " << (3 >= 2) << endl;
+//
+//    cout<<"3 >= 4 = " << (3 >= 4) << endl;
+//
+//    cout<<"4 == 3 = " << (4 == 3) << endl <<endl;
+
+
+
+
+
+//Phase 6:
+//    int num;
+//
+//    cout<<"Please enter a number: ";
+//
+//    cin>> num;
+//
+//    if (num = 2)
+//    {
+//        cout<<"Your number is equal to 2! ;) " <<endl;
+//    }
+//    else
+//    {
+//        cout<<"Your number is not equal to 2! ;) " <<endl;
+//    }
+
+
+
+
+    //Phase 2 :
+
+//    cout<<"Is (23 > 3) ? Answer: " << (23 > 3) << endl;
+//
+//    cout<<"Is (12 > 233) ? Answer: " << (12 > 233) << endl;
+//
+//    cout<<"Is (4 == 4) ? Answer: " << (4 == 4) << endl;
+//
+//    cout<<"Is (4 <= 4) ? Answer: " << (4 <= 4) << endl;
+//
+//    cout<<"Is (4 >= 4) ? Answer: " << (4 >= 4) << endl;
+//
+//    cout<<"Is (3 >= 2) ? Answer: " << (3 >= 2) << endl;
+//
+//    cout<<"Is (3 >= 4) ? Answer: " << (3 >= 4) << endl;
+//
+//    cout<<"Is (4 == 3) ? Answer: " << (4 == 3) << endl <<endl;
+//
+//
+//
+
+
+
+
+
+
+    //Phase 3:
+
+
+//    int percentage_marks;
+//
+//    cout<<"Please your percentage marks: " << endl;
+//
+//    cin>> percentage_marks;
+//
+//    if (percentage_marks > 90)
+//    {
+//        cout<<"You got 1st class grades! :0 " <<endl;
+//    }
+//    else if (percentage_marks > 50)
+//    {
+//        cout<<"You got 2nd class grades! :> " <<endl;
+//    }
+//    else
+//    {
+//        cout<<"You got 3rd class grades! :3 " << endl;
+//    }
+//
+
+
+
+
+    //Phase 4:
+
+//    cout<<"Please enter a number: " ;
+//
+//    int no;
+//
+//    cin >> no;
+//
+//    if (no % 2 == 0)
+//    {
+//        if (no % 5 == 0)
+//        {
+//            cout<<endl<<no << " is divisible by both 2 and 5. " << endl;
+//        }
+//
+//        else
+//        {
+//            cout<< endl << no << " is only divisible by 2. " << endl;
+//        }
+//    }
+//    else if (no % 5 == 0)
+//    {
+//        cout<<endl<< no << " is only divisible by 5. " <<endl;
+//    }
+//    else
+//    {
+//        cout<<endl << no <<" is not divisible by either 2 nor 5. \nFTP!" <<endl;
+//    }
+//
+
+
+
+
+
+    //Phase 5:
+//    cout<<"Please enter a number: " ;
+//
+//    int no;
+//
+//    cin >> no;
+//
+//    if (no % 2 == 0 && no % 5 == 0)
+//    {
+//        cout<<endl<<no << " is divisible by both 2 and 5. " << endl;
+//
+//    }
+//    else if (no % 2 == 0)
+//    {
+//        cout<< endl << no << " is only divisible by 2. " << endl;
+//    }
+//    else if (no % 5 == 0)
+//    {
+//        cout<<endl<< no << " is only divisible by 5. " <<endl;
+//    }
+//    else
+//    {
+//        cout<<endl << no <<" is not divisible by either 2 nor 5. \nFTP!" <<endl;
+//    }
+
+
+
+
+
+
+//    //Phase 7
+//    cout<<"Please enter a number: ";
+//
+//    int your_number_variable;
+//
+//    cin>> your_number_variable;
+//
+//
+//    if (your_number_variable % 2 == 0 || your_number_variable % 7 == 0)
+//    {
+//        cout<<"\nYour number is divisible by either 2 or 7. " <<endl;
+//    }
+//    else
+//    {
+//        cout<<"\nYour number is not divisible by either 2 nor 7. " <<endl;
+//    }
+
+
+
+
+
+  //Phase 8
+//    cout<<"Please enter a number: ";
+//
+//    int your_number_variable;
+//
+//    cin>> your_number_variable;
+//
+//
+//    if (your_number_variable % 2 == 0 || your_number_variable % 7 == 0)
+//    {
+//        cout<<"\nYour number is divisible by either 2 or 7. " <<endl;
+//
+//        cout<<"\nNow lets check which one it is... " << endl;
+//
+//        if (your_number_variable % 2 == 0)
+//        {
+//            cout<<"\nYour number was divisible by 2. " <<endl;
+//        }
+//        else if (your_number_variable % 7 == 0)
+//        {
+//            cout<<"\nYour number was divisible by 7. " <<endl;
+//        }
+//    }
+//    else
+//    {
+//        cout<<"\nYour number is not divisible by either 2 nor 7. " <<endl;
+//    }
+
+
+
+    return 0;
+}
