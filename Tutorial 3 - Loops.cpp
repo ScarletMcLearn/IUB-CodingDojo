cd2a79658903ce8cfd41c5ad28161a51797a3caa
ScarletMcLearn <ratul_shams@yahoo.com>
2017-05-24 15:04:23 -0400

    Add files via upload

---
+++ Tutorial 3 - Loops.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+
+#include <cstdlib>
+
+#include <cmath>
+
+using namespace std;
+
+
+
+int main()
+{
+
+//     cout<<"Accha lets try something new." <<endl;
+//
+//     cout<<"\nAmar Akta friend ase, oy onek bachal.\n\n";
+//
+//     cout<<"Or mukh khullei khali..."<<endl;
+//
+//     cout<<"Press 'ki' to find out what: ";
+//
+//     string input;
+//
+//     cin>> input;
+//
+//
+//     int terminating_value = -1;
+//
+//     if (input == "ki")
+//     {
+//         cout<<endl<<"\nHetey khali bole ";
+//
+//         for (int counter_variable = 0; counter_variable > terminating_value; counter_variable = counter_variable + 1)
+//         {
+//             cout<<"pek ";
+//
+// //            waiting(5);
+//         }
+//     }
+//     else
+//     {
+//         cout<<endl<<"For Loop-er Structure: \nfor (int counter_variable = initial_value_of_counter_variable; terminating_condition_of_the_loop; increment_or_decrement_or_divide_or_multiply_to_reach_there)\n{\n\t\\\\Body of Loop.;\n}" <<endl;
+//     }
+
+
+
+
+
+
+
+
+
+
+
+//     cout<<endl<<"Lets try another loop: ";
+//
+//     cout<<endl;
+//
+//     cout<<"\nAmar Arekta friend ase, oy onek bachal.\n\n";
+//
+//     cout<<"Or mukh khullei khali..."<<endl;
+//
+//     cout<<"Press 'ki' to find out what: ";
+//
+//     string input;
+//
+//     cin>> input;
+//
+//     cout<<endl;
+//
+//     int terminating_value = -1;
+//
+//     if (input == "ki")
+//     {
+//         cout<<endl<<"\nHetey khali bole ";
+//
+//         int counter_variable = 0;
+//
+//         while (counter_variable > terminating_value)
+//         {
+//             cout<<"pek ";
+//
+// //            waiting(5);
+//             counter_variable = counter_variable + 1;
+//         }
+//     }
+//     else
+//     {
+//         cout<<endl<<"\nWhile Loop-er Structure: \nint counter_variable = initial_value_of_counter_variable; \n\nwhile (condition for while loop, i.e.  terminating_condition_of_the_loop)\n{\n\t\\\\Body of Loop.;\n \\\\increment_or_decrement_or_divide_or_multiply_counter_to_reach_terminating_condition)\n}\n\n" <<endl;
+//     }
+
+
+
+
+
+
+
+
+// int number;
+//
+// cout<<"lta nambar dao jiru baade: ";
+//
+// cin>> number;
+//
+//
+// while(number != 0)
+// {
+//     cout<<"\n\nTomar nambaar: " << number <<endl<<endl;
+//
+//     cout<<"1ta nambar dao jiru baade: ";
+//
+//     cin>> number;
+// }
+
+
+
+
+
+
+
+
+
+
+
+//
+// int number;
+//
+// do
+// {
+//
+//
+//     cout<<"1ta nambar dao jiru baade: ";
+//
+//     cin>> number;
+//
+//     cout<<"\n\nTomar nambaar: " << number <<endl<<endl;
+//
+// } while(number != 0);
+//
+//
+//
+// cout<<endl<<endl;
+// cout<<"Do-While Loop-Er Structure:\n\n";
+// cout<<"do\n{\n\t//Body of Do-While Loop;\n\n} while(terminating_condition_of_the_loop);\n\n";
+
+
+	return 0;
+}
