637bc22508880a966cecc8e2270d4cfbdd95bdd3
ScarletMcLearn <ratul_shams@yahoo.com>
2017-07-05 21:37:02 +0600

    Add files via upload

---
+++ Tutorial 4 - Random Number and Array.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+#include <cstdlib>
+//#include <ctime>
+
+using namespace std;
+
+main()
+{
+    int lower_lim,  upper_lim;
+
+    srand(time(0));
+//
+//    cout <<"LL UL " ;
+//
+//    cin>> lower_lim >> upper_lim;
+//
+//
+//	for (int i = 0; i < 10; i++)
+//    {
+//            cout << (lower_lim + rand() % (upper_lim - lower_lim + 1) ) << " ";
+//    }
+
+
+
+
+//    int Int_Array[1];
+//
+//    cout <<"Size of integer: " << sizeof(int) << endl;
+//    cout <<"Size of integer: " << sizeof(Int_Array) << endl;
+
+
+
+     int Int_Array[2];
+
+    cout <<"Size of integer: " << sizeof(int) << endl;
+    cout <<"Size of integer: " << sizeof(Int_Array) << endl;
+
+
+    int Array[3] = {1, 2, 3};
+
+
+
+    cout <<"Array: " << Array << endl;
+
+
+
+
+//    cout <<"Array's Memory Location: " << &Array << endl;
+
+
+
+//    for (int i = 0; i < 3; i++)
+//    {
+//        cout << "Element in " << i << " Number Index: " << Array[i] << "\n";
+//    }
+
+
+
+
+//    for (int i = 0; i < 3; i++)
+//    {
+//        cout << "Please enter Element in " << i << " Number Index: " << Array[i] << "\n";
+//
+//        cin>>Array[i];
+//    }
+//
+//
+//
+//    cout << endl;
+//
+//    for (int i = 0; i < 3; i++)
+//    {
+//        cout << "Element in " << i << " Number Index: " << Array[i] << "\n";
+//    }
+
+
+    return 0;
+}
